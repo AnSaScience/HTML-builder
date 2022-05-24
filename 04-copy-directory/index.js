4e1214ddf480d7327ad8951b1c8e91bbbb899d99
AnSaScience <ansascience@gmail.com>
2022-05-24 22:27:33 +0300

    fix: fix remove of deleted files from copied folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 const copyDirPath = path.join(__dirname,'files');
 const copiedDirPath = path.join(__dirname,'files-copy');
@@ -10,16 +11,36 @@ function copyDir(copyDirPath,copiedDirPath){
   fs.mkdir(copiedDirPath,(err)=>{if(err){
     return;
   }
+  // fsPromises.mkdir(copiedDirPath,{recursive:true});
   });
   
   //copy files if do not exist
   fs.readdir(copyDirPath,{ withFileTypes: true },(err,files)=>{
+    //create array of file names:
+    const copyFileNames=files.map(file=>{
+      return file.name;
+    })
+    console.log(copyFileNames);
+
+    //find out if the folder is empty
+    fs.readdir(copiedDirPath,{ withFileTypes: true },(err,data)=>{
+      if(data.length){
+        data.forEach(item=>{
+          if(!copyFileNames.includes(item.name)){
+            fs.rm(path.join(copiedDirPath,item.name),(err)=>{
+              if(err) throw err;
+            });
+          }
+        });
+      }
+    });
+
     files.forEach(file=>{
       if(file.isFile()){
         let copyFilePath=path.join(copyDirPath,`${file.name}`);
         let copiedFilePath=path.join(copiedDirPath,`${file.name}`);
-        fs.cp(copyFilePath,copiedFilePath,{force:false},(err)=>{
-          if(err) throw err;
+        fs.cp(copyFilePath,copiedFilePath,{force:true},(err)=>{
+          // if(err) throw err;
         });
       }
     });  
